bb62c36ccf6f5cc0dcbcfb50303fd2142dddbd8d
tslil <tslilcrispel@yahoo.com>
2024-08-11 22:07:21 +0300

    comparator to lower case

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
 const ID_SEPARATOR = '<<>>'
 
 const DICTIONARY = {
-    'I.R.R.': 'IRR'
+    'I.R.R.': 'IRR',
+    'red team-cyber posture': 'red team'
 }
 
 export const createItemId = (part1, part2) => {
     const part1AfterDict = DICTIONARY[part1] || part1
     const part2AfterDict = DICTIONARY[part2] || part2
-    return `${part1AfterDict}${ID_SEPARATOR}${part2AfterDict}`
+    return `${part1AfterDict.toLowerCase()}${ID_SEPARATOR}${part2AfterDict.toLowerCase()}`
 }
 
 export const destructID = (id) => {
