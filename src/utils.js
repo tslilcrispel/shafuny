384d42395dd58939373bc0f6601675c6d60d526d
tslil <tslilcrispel@yahoo.com>
2024-08-11 22:15:06 +0300

    lil fix

--- src/utils.js
+++ src/utils.js
@@ -6,8 +6,8 @@ const DICTIONARY = {
 }
 
 export const createItemId = (part1, part2) => {
-    const part1AfterDict = DICTIONARY[part1] || part1
-    const part2AfterDict = DICTIONARY[part2] || part2
+    const part1AfterDict = DICTIONARY[part1] || part1 || ''
+    const part2AfterDict = DICTIONARY[part2] || part2 || ''
     return `${part1AfterDict.toLowerCase()}${ID_SEPARATOR}${part2AfterDict.toLowerCase()}`
 }
 
